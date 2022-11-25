6b1b5e1a361c2a51bfbdd9e6ba95885601fab8fe
Vikram Kumar <104130605+Vikramkumarcoder@users.noreply.github.com>
2022-11-25 17:48:39 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+	int x,y,z;
+	cin>>x;
+	cin>>y;
+	cout<<x*y<<endl;
+	int acii = 'U';
+	cout<<acii<<endl;
+	cout<<"The ACII value of U:  "<<int('U')<<endl;
+	cin>>z;
+	cout<<z*z*z;
+	cout<<sizeof(char);
+	return 0;
+}
